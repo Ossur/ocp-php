387ba7ec0be5eb41c3e34b6772903fca71fa9405
Össur Ingi <ossurj@tmsoftware.is>
2017-05-19 09:58:24 +0000

    Ay

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
  <head>
  </head>
  <body>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 259.91937 212.55713" >
+		<path d="M13 213c0-156 235-155 234 0M0 13h259M130 213l-1-200" 
+			stroke="#28a9d9" fill="none" strokeWidth="25"
+		/> 
+	</svg>
+
  <?php echo 'App Version: 10.2. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
